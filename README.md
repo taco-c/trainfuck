27dc53801e04653f6417fc7c70ea59ff99e0f597
Taco-C <5463191+taco-c@users.noreply.github.com>
2018-12-12 02:04:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Trainfuck
+
+Trainfuck is a interpreter based on the
+[Brainfuck][https://en.wikipedia.org/wiki/Brainfuck] language, but the Trainfuck
+language differs in a few ways:
+
+* Numbers! Instead of writing `++++++++++`, you can write `+10`. This also works
+    for `-`, `>`, and `<`.
+* One more output char: :! Colon prints the numerical value of the cell instead
+    of the ASCII representation.
+* It doesn't have comma.
+
+## How to run
+
+The only part of this README you will read:
+
+```
+./trainfuck.py <filename>
+```
+
+For the file extension I like to use `.tf` (if Brainfuck is `.bf`). But don't let
+that stop you from using something more fun, like `.trainfuck`
+
+## Hello World
+
+```
++8[>+4[>++>+3>+3>+<4-]>+>+>->>+[<]<-]>>.>-3.+7..+3.>>.<-.<.+3.-6.-8.>>+.>++.
+```
+It also supports:
+```
+++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.++++
++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
+```
+or any variations.
+
+## Compatibility with Brainfuck
+
+Should be able to execute Brainfuck code as long as:
+
+* Code does not rely on the comma.
+* Comments don't contain any numbers or colon (Except in a comment at the top
+    inside a pair of []'s).
---
+++ trainfuck.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+""" Trainfuck.
+    
+    Usage: trainfuck.py <filename>
+"""
+
+""" The meat of Trainfuck. """
+def interpret (code):
+    code = prepare_code(code)
+    cells, cell_ptr, code_ptr = [0], 0, 0
+    loops = prepare_loops(code)
+    
+    while code_ptr < len(code):
+        key, value = code[code_ptr]
+        
+        if key == "+":
+            cells[cell_ptr] = cells[cell_ptr] + value
+            
+            if cells[cell_ptr] > 255:
+                cells[cell_ptr] = cells[cell_ptr] - 256
+        
+        elif key == "-":
+            cells[cell_ptr] = cells[cell_ptr] - value
+            
+            if cells[cell_ptr] < 0:
+                cells[cell_ptr] = 256 + cells[cell_ptr]
+        
+        elif key == ">":
+            cell_ptr += value
+            
+            if cell_ptr == len(cells):
+                cells.append(0)
+        
+        elif key == "<":
+            cell_ptr = cell_ptr - value if cell_ptr - value > 0 else 0
+        
+        elif key == ".":
+            print(chr(cells[cell_ptr]), end="")
+        
+        elif key == ":":
+            print(cells[cell_ptr], end="")
+        
+        elif key == "[":
+            if cells[cell_ptr] == 0:
+                code_ptr = loops[code_ptr]
+                continue
+        
+        elif key == "]":
+            if cells[cell_ptr] != 0:
+                code_ptr = loops[code_ptr]
+                continue
+
+        code_ptr += 1
+
+""" This function writes down the location off all the loops (both the start
+    and the end) in the train-/brainfuck file. And then returns his notebook
+    to the place that asked for it. This is a good boy. """
+def prepare_loops (code):
+    loop_starts = []
+    loops = {}
+    i = 0
+    
+    for key, value in code:
+        if key == "[":
+            loop_starts.append(i)
+        
+        elif key == "]":
+            start = loop_starts.pop()
+            loops[i] = start
+            loops[start] = i
+        
+        i += 1
+    
+    return loops
+
+""" Trainfuck is picky about its inputs. This function strips off all
+    non-essential characters, and returns a list of tuples. """
+def prepare_code (code_str):
+    SYMBOLS = ["+", "-", ">", "<", ".", ":", "[", "]"]
+    NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
+    prepared_code = []
+    code = "".join([x for x in code_str if x in SYMBOLS + NUMBERS])
+    
+    i = 0
+    
+    while True:
+        if i >= len(code):
+            break
+        
+        value = ""
+        j = i + 1
+        
+        if j >= len(code):
+            prepared_code.append((code[i], 1))
+            i += 1
+            
+            continue
+        
+        if code[j] in SYMBOLS:
+            prepared_code.append((code[i], 1))
+            i += 1
+            
+            continue
+        
+        while code[j] in NUMBERS:
+            value += code[j]
+            j += 1
+            if j >= len(code):
+                break
+        
+        if value != "":
+            prepared_code.append((code[i], int(value)))
+        
+        i += j - i
+        
+    return prepared_code
+
+""" This comment is here to visually separate the above function from 
+    this thing. """
+if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) < 2:
+        print("Usage:", sys.argv[0], "<filename>")
+        exit(1)
+    
+    try:
+        with open(sys.argv[1]) as f:
+            code = f.read()
+    
+    except IOError as e:
+        print(e)
+        exit(1)
+    
+    interpret(code)
